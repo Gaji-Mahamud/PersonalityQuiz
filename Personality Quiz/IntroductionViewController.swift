ce938104cf8a652a03f8a958fcdbeb1e1db84238
Gaji Mahamud <gmm182@scarletmail.rutgers.edu>
2022-03-14 17:29:49 -0400

    Initial Commit

---
+++ Personality Quiz/IntroductionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by Gaji Mahamud on 3/7/22.
+//
+// “On my honor, I have neither received nor given any unauthorized assistance on Mid Term Exam." - Gaji Mahamud
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+    }
+}
+
